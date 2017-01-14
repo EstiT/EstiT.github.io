edbd45c5da10c5928353ce8f9c0112d93c7ddfe7
Esti Tweg <estictweg@hotmail.com>
2017-01-13 22:52:38 -0500

    html: 	linked js 		made about me links have images appear on hover/click 		deleted Gokhale posture workshop from education 		removed CCC tank top from portfolio 		updated Pole fitness logo description
    Style:	made images for about me hidden
    		adjusted height of education page
    		deleted styling for images no longer there
    
    JS: 		made profile picture corner radius adjust to image size
    		made about me links have images fade in on mouse over

---
+++ script.js
@@ -0,0 +1,29 @@
+window.addEventListener('load',function()
+{
+
+  var me = document.getElementById('me');
+  console.log("width:"+me.clientWidth);
+  me.style.borderRadius = me.clientWidth/2+"em";
+
+  $('#australia').append("<img id = 'aussie' src='Australia.jpg' height='100px' width='120px' style='position: absolute; z-index: 3; display: none'>");
+
+  $("#australia").mouseover(function(){
+    console.log("moused over");
+    $("#assie").css.display = "block";
+
+  });
+  $( "#australiaLink" ).mouseover(function() {
+    $( "#australia" ).fadeIn( 175 );
+});
+
+$( "#climbingLink" ).mouseover(function() {
+  $( "#climbing" ).fadeIn( 175 );
+});
+
+$( "#worldsLink" ).mouseover(function() {
+  $( "#worlds" ).fadeIn( 175 );
+});
+
+
+
+});
