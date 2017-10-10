15d1be6771d00e42a95087166ef10681c4e1eaa0
Esti Tweg <estictweg@hotmail.com>
2017-10-10 16:54:19 -0400

    Removed profile picture. Made favicon.ico in root folder. Fixed image path issues.

--- script.js
+++ script.js
@@ -4,12 +4,9 @@ var purple  = "rgba(48, 54, 103, 0.8)";
 
 
 
-window.addEventListener('load',function()
-{
-  var me = document.getElementById('me');
-  me.style.borderRadius = me.clientWidth/2+"em";
-
-  $('#australia').append("<img id = 'aussie' src='Australia.jpg' height='100px' width='120px' style='position: absolute; z-index: 3; display: none'>");
+window.addEventListener('load',function(){
+   $("#homeM").css({ "border-bottom": "4px solid "+pink});
+  $('#australia').append("<img id = 'aussie' src='images/Australia.jpg' height='100px' width='120px' style='position: absolute; z-index: 3; display: none'>");
 
   $( "#australiaLink" ).mouseover(function() {
     $( "#australia" ).fadeIn( 175 );
