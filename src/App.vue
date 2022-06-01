0ad4de9974daed1a28326052151f89f7dc2230cc
Esti <estictweg@hotmail.com>
2022-06-01 23:19:18 +0930

    catch errors

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 	<SiteTemp v-else />
 </template>
 
+
 <script setup>
 import SiteTemp from './views/SiteTemp.vue'
 import Results from './views/Results.vue';
@@ -13,6 +14,12 @@ const isResults = computed(() => window?.location?.pathname?.includes('results')
 console.log(window.location.href)
 console.log(isResults.value)
 if (isResults.value) window.history.pushState("", "", "/" + window.location.href.substring(window.location.href.lastIndexOf('/') + 1).split("?")[0]);
+
+window.addEventListener('error', function (e) {
+	console.log(e, window.location.href, isResults);
+	if (isResults.value) window.history.pushState("", "", "/" + window.location.href.substring(window.location.href.lastIndexOf('/') + 1).split("?")[0]);
+
+}, true);
 </script>
 
 <style>
